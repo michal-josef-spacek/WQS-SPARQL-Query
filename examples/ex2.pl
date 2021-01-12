ad5c59eea68b46513250d285f62b8f49faa04b84
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-12 23:31:13 +0100

    Add Select helper

--- examples/ex2.pl
+++ examples/ex2.pl
@@ -3,13 +3,13 @@
 use strict;
 use warnings;
 
-use Wikibase::WQS::SPARQL::Query::Select;
+use Wikibase::WQS::SPARQL::Query::Count;
 
-my $obj = Wikibase::WQS::SPARQL::Query::Select->new;
+my $obj = Wikibase::WQS::SPARQL::Query::Count->new;
 
 my $property = 'P957';
 my $isbn = '80-239-7791-1';
-my $sparql = $obj->select_simple($property, $isbn);
+my $sparql = $obj->count_simple($property, $isbn);
 
 print "Property: $property\n";
 print "ISBN: $isbn\n";
@@ -20,6 +20,6 @@ print $sparql;
 # Property: P957
 # ISBN: 80-239-7791-1
 # SPARQL:
-# SELECT ?item WHERE {
+# SELECT (COUNT(?item) as ?count) WHERE {
 #   ?item wdt:P957 '80-239-7791-1'
 # }
---
+++ t/Wikibase-WQS-SPARQL-Query-Select/04-count_simple.t
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+use Test::More 'tests' => 2;
+use Test::NoWarnings;
+use Wikibase::WQS::SPARQL::Query::Select;
+
+# Test.
+my $obj = Wikibase::WQS::SPARQL::Query::Select->new;
+my $property = 'P957';
+my $isbn = '80-239-7791-1';
+my $sparql = $obj->select_simple($property, $isbn);
+my $right_ret = <<"END";
+SELECT ?item WHERE {
+  ?item wdt:$property '$isbn'
+}
+END
+is($sparql, $right_ret, 'Simple SPARQL select query.');
