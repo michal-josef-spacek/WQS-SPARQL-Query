de5df6692c33d0230479355b7ac2b8c6db851b85
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-12 20:52:46 +0200

    Prepare for 0.03 version

--- Query/Count.pm
+++ Query/Count.pm
@@ -6,7 +6,7 @@ use warnings;
 use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub new {
 	my ($class, @params) = @_;
@@ -203,6 +203,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
