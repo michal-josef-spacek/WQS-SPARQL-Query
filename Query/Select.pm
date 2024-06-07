b98d3019670fca82b74947c35c40cbd28c598948
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-06-07 10:52:32 +0200

    Fix escape sequences in Select

--- Query/Select.pm
+++ Query/Select.pm
@@ -41,8 +41,11 @@ sub select_value {
 		} elsif ($value =~ m/^\?/ms) {
 			# same
 		} elsif ($value =~ m/^(.*?)(@\w\w)$/ms) {
-			$value = "'$1'$2";
+			my ($main_value, $lang) = ($1, $2);
+			$main_value =~ s/'/\\'/msg;
+			$value = "'$main_value'$lang";
 		} else {
+			$value =~ s/'/\\'/msg;
 			$value = "'$value'";
 		}
 
